b95865f59a9057475cd1d11fe79927bddc31eb80
Robert Coffie <rcoffie22@yhahoo.com>
2022-09-19 08:34:22 +0000

    first commit

---
+++ head_start/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+//   write a while loop that give output of DooBeeDooBeeDo
+        int x = 1;
+        while (x < 3) {
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x = x + 1;
+        }
+        if (x == 3) {
+            System.out.print("Do");
+        }
+    }
+
+
+}
